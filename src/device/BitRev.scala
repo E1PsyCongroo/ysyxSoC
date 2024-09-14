72b07b4a7c394059bb65339afd7a6e6c81fbc8d2
focused_xy <elpsycongroo242@gmail.com>
2024-09-14 11:03:04 +0800

    feat(flash&bitrev): spi with xip

--- src/device/BitRev.scala
+++ src/device/BitRev.scala
@@ -2,6 +2,7 @@ package ysyx
 
 import chisel3._
 import chisel3.util._
+import os.isFile
 
 class bitrev extends BlackBox {
   val io = IO(Flipped(new SPIIO(1)))
@@ -9,5 +10,15 @@ class bitrev extends BlackBox {
 
 class bitrevChisel extends RawModule { // we do not need clock and reset
   val io = IO(Flipped(new SPIIO(1)))
-  io.miso := true.B
+
+withClockAndReset(io.sck.asBool.asClock, io.ss.asBool.asAsyncReset) {
+  val shiftReg  = RegInit(0.U(8.W))
+  val bitRevReg = WireDefault(0.U(8.W))
+  val count     = RegInit(0.U(4.W))
+  count := count + 1.U
+
+  shiftReg  := Mux(!count(3), io.mosi ## shiftReg(7, 1), shiftReg)
+  bitRevReg := Reverse(shiftReg)
+  io.miso   := Mux(count(3), bitRevReg(count(2, 0)), true.B)
+}
 }
--- src/device/SPI.scala
+++ src/device/SPI.scala
@@ -46,7 +46,90 @@ class APBSPI(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModul
     val mspi = Module(new spi_top_apb)
     mspi.io.clock := clock
     mspi.io.reset := reset
-    mspi.io.in <> in
+
+    val fire          = mspi.io.in.penable && mspi.io.in.pready
+    val isSPIAccess   = address(0).contains(in.paddr)
+    val isFlashAccess = address(1).contains(in.paddr)
+    assert(!(isFlashAccess && in.pwrite))
+
+    val sIdle :: sAccessSPI :: sAccessTx :: sAccessDivider :: sAccessSs :: sAccessCtrl :: sWaitData :: sSendBack :: Nil = Enum(8)
+    val state = RegInit(sIdle)
+    state := MuxLookup(state, sIdle)(Seq(
+      sIdle           -> MuxCase(sIdle, Seq(
+        (in.psel && isSPIAccess)    -> sAccessSPI,
+        (in.psel && isFlashAccess)  -> sAccessTx,
+      )),
+      sAccessSPI      -> Mux(fire, sIdle, sAccessSPI),
+      sAccessTx       -> Mux(fire, sAccessDivider, sAccessTx),
+      sAccessDivider  -> Mux(fire, sAccessSs, sAccessDivider),
+      sAccessSs       -> Mux(fire, sAccessCtrl, sAccessSs),
+      sAccessCtrl     -> Mux(fire, sWaitData, sAccessCtrl),
+      sWaitData       -> Mux(fire && !mspi.io.in.prdata(8), sSendBack, sWaitData),
+      sSendBack       -> Mux(fire, sIdle, sSendBack),
+    ))
+
+    val isIdle          = state === sIdle
+    val isAccessSPI     = state === sAccessSPI
+    val isAccessTx      = state === sAccessTx
+    val isAccessDivider = state === sAccessDivider
+    val isAccessSs      = state === sAccessSs
+    val isAccessCtrl    = state === sAccessCtrl
+    val isWaitData      = state === sWaitData
+    val isSendBack      = state === sSendBack
+
+    val SPIReq = Wire(new APBBundle(APBBundleParameters(addrBits = 32, dataBits = 32)))
+    SPIReq.psel     := Mux(isIdle, false.B, in.psel)
+    SPIReq.penable  := Mux(isIdle, false.B, in.penable)
+    SPIReq.pwrite   := MuxCase(in.pwrite, Seq(
+      isAccessTx      -> true.B,
+      isAccessDivider -> true.B,
+      isAccessSs      -> true.B,
+      isAccessCtrl    -> true.B,
+      isWaitData      -> false.B,
+      isSendBack      -> false.B,
+    ))
+    SPIReq.paddr    := MuxCase(in.paddr, Seq(
+      isAccessTx      -> "h1000_1004".U,
+      isAccessDivider -> "h1000_1014".U,
+      isAccessSs      -> "h1000_1018".U,
+      isAccessCtrl    -> "h1000_1010".U,
+      isWaitData      -> "h1000_1010".U,
+      isSendBack      -> "h1000_1000".U,
+    ))
+    SPIReq.pprot    := in.pprot
+    SPIReq.pwdata   := MuxCase(in.pwdata, Seq(
+      isAccessTx      -> "h03".U(8.W) ## in.paddr(23, 0),
+      isAccessDivider -> "h0000_0001".U,
+      isAccessSs      -> "h0000_0001".U,
+      isAccessCtrl    -> "h0000_2540".U,
+    ))
+    SPIReq.pstrb    := MuxCase(in.pstrb, Seq(
+      isAccessTx      -> "b1111".U,
+      isAccessDivider -> "b0011".U,
+      isAccessSs      -> "b0001".U,
+      isAccessCtrl    -> "b1111".U,
+    ))
+    SPIReq.pauser   := DontCare
+
+    SPIReq.pready   := Mux(isAccessSPI || isSendBack, mspi.io.in.pready, false.B)
+    SPIReq.pslverr  := mspi.io.in.pslverr
+    val littleEndianData = mspi.io.in.prdata(7, 0) ## mspi.io.in.prdata(15, 8) ## mspi.io.in.prdata(23, 16) ## mspi.io.in.prdata(31, 24)
+    SPIReq.prdata   := Mux(isSendBack, littleEndianData, mspi.io.in.prdata)
+    SPIReq.pduser   := DontCare
+
+    mspi.io.in.psel     := SPIReq.psel
+    mspi.io.in.penable  := SPIReq.penable
+
+    mspi.io.in.pwrite   := SPIReq.pwrite
+    mspi.io.in.paddr    := SPIReq.paddr
+    mspi.io.in.pprot    := SPIReq.pprot
+    mspi.io.in.pwdata   := SPIReq.pwdata
+    mspi.io.in.pstrb    := SPIReq.pstrb
+
+    in.pready           := SPIReq.pready
+    in.pslverr          := SPIReq.pslverr
+    in.prdata           := SPIReq.prdata
+
     spi_bundle <> mspi.io.spi
   }
 }
